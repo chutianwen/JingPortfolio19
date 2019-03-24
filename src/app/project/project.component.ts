e36c6c5fe07d42d51ecc84ed7d361446f130111d
tianwenchu <tianwen.chu@fractalindustries.com>
2019-03-24 11:34:04 -0400

    v1.2

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+// export declare function projectBGAnimationController(): any;
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -10,6 +12,7 @@ export class ProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // triBackgroundAnimationController();
   }
 
 }
